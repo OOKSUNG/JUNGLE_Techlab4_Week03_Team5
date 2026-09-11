8341b2b60e702389b28e89a1365bf2a88360663d
youngho <hengjegi1221@gmail.com>
2026-09-11 16:20:59 +0900

    docs: ShowFlags 함수 주석 추가

--- EngineProject/Engine/ShowFlags.h
+++ EngineProject/Engine/ShowFlags.h
@@ -19,7 +19,11 @@ struct FEngineShowFlags
 {
 	uint64 Flags = static_cast<uint64>(EShowFlagBits::Default);
 
-	// 설정
+	/**
+	* @brief ShowFlag에서 특정 Flag를 On/Off로 설정합니다.
+	* @param Flag 설정하고자 하는 Flag의 열거형 값
+	* @param bValue 활성화 여부 (true: On, false: Off)
+	*/
 	void Set(EShowFlagBits Flag, bool bValue)
 	{
 		if (bValue)
@@ -27,20 +31,28 @@ struct FEngineShowFlags
 		else
 			Flags &= ~static_cast<uint64>(Flag);
 	}
-	//토글
+	/**
+	* @brief ShowFlag에서 특정 Flag의 상태를 토글(반전)합니다.
+	* @param Flag 반전하고자 하는 Flag의 열거형 값
+	*/
 	void Toggle(EShowFlagBits Flag)
 	{
 		Flags ^= static_cast<uint64>(Flag);
 	}
-	// 확인 IsSet
+	/**
+	* @brief 특정 Flag가 활성화되어 있는지 확인합니다.
+	* @param Flag 확인할 Flag의 열거형 값
+	* @return 해당 Flag가 켜져 있으면 true, 꺼져 있으면 false
+	*/
 	bool IsSet(EShowFlagBits Flag)
 	{
 		return ((Flags & static_cast<uint64>(Flag)) != 0);
 	}
-	// 신 리셋시 기본값 SetDefault
+	/**
+	* @brief 씬 시작 시 전체 플래그의 값을 기본값(Default)으로 재설정합니다.
+	*/
 	void SetDefault()
 	{
 		Flags = static_cast<uint64>(EShowFlagBits::Default);
 	}
-
 };
