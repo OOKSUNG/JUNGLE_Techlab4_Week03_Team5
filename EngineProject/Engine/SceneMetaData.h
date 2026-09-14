3d79749c982aef3bd735e45bf71f1fd242729325
Sangyun Jeong <jsangyun1039@gmail.com>
2026-09-14 22:44:43 +0900

    Refactor: FSceneMetaData 구조체 수정

--- EngineProject/Engine/SceneMetaData.h
+++ EngineProject/Engine/SceneMetaData.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "Core/Types.h"
-#include "GameFramework/Actor.h"
+#include "Core/Containers.h"
+#include "Math/Transform.h"
+#include "Core/EngineString.h"
 
 struct FSceneMetaData
 {
 	int Version;
 	uint32 NextUUID;
-	TArray<AActor*> Actors;
+	TArray<uint32> UUIDs;
+	TMap<uint32, FTransform> Transforms;
+	TMap<uint32, FString> Types;
 };
